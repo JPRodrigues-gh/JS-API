278537fcfd238ace8c260a45dafa6a0595ac4dab
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-06-13 04:00:57 +0000

    Add displayErrors function for postForm. Add processOptions function frame

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,62 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions (form) {}
+
+async function postForm(e) {
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
+    for (let entry of form.entries()) {
+        console.log(entry);
+    }
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        result = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        result = `<div>Total errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            result += `<div>At line <span class="line">${error.line}</span>, `;
+            result += `column <span class="column">${error.col}</span></div>`;
+            result += `<div class="error">${error.error}</div>`;
+        }
+    }
+    const resultTitle = document.getElementById("resultsModalTitle");
+    const resultContent = document.getElementById("results-content");
+
+    resultTitle.innerText = heading;
+    resultContent.innerHTML = result;
+    resultsModal.show();
+}
+
 async function getStatus(e) {
     /** Concatenate the API URL and KEY using string literals and in the
         format provided JSHINT API documentation for the GET request */
     var keyUrlString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(keyUrlString);
-    
+
     const data = await response.json();
 
-    if(response.ok) {
+    if (response.ok) {
         displayStatus(data);
     } else {
         throw new Error(data.error);
@@ -31,5 +77,5 @@ function displayStatus(data) {
     resultTitle.innerText = title;
     resultContent.innerHTML = result;
     resultsModal.show();
-    
+
 }
